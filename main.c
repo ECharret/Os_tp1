7c49196ad3fb85a33fa2fa6a021520d591516b06
ECharret <127148643+ECharret@users.noreply.github.com>
2024-10-09 11:30:07 +0200

    Update main.c Q2

--- main.c
+++ main.c
@@ -5,17 +5,19 @@
 typedef struct HEADER_TAG {
 	struct HEADER_TAG * ptr_next; /* pointe sur le prochain bloc libre */
 	size_t bloc_size; /* taille du memory bloc en octets*/
-	long magic_number; /* 0x0123456789ABCDEF */
+	long magic_number; /* 0x0123456789ABCDEFL*/
 } HEADER;
 
+HEADER *freeliste=NULL;
+
 void * malloc_3is(ssize_t x){
-	printf("beginning : %p\n",sbrk(0));
 	HEADER *head=sbrk(sizeof(HEADER)+x+sizeof(long));
 	head->ptr_next=NULL;
 	head->bloc_size=x;
-	head->magic_number=0x0123456789ABCDEF;
+	head->magic_number=0x0123456789ABCDEFL;
 	void *pointeurx=head+1;
 	void *magic=sbrk(0)-sizeof(long);
+	printf("beginning : %p\n",pointeurx-sizeof(HEADER));
 	printf("beginning of x: %p\n",pointeurx);
 	printf("beginning of magic number: %p\n",magic);
 	printf("size of x: %li\n",magic-pointeurx);
@@ -23,9 +25,18 @@ void * malloc_3is(ssize_t x){
 };
 
 void free_3is(void *ptr){
-	
+	HEADER *head=ptr-sizeof(HEADER);
+	head->ptr_next=freeliste;
+	freeliste=head;
+	printf("freeliste: %p \n",freeliste);
 	return;
 };
+
+int main(){
+	void *test=malloc_3is(200);
+	free_3is(test);
+	return 1;
+}
 
 int main(){
 	void *test=malloc_3is(200);
